17a298ce4434b38704a96fc18765c7079d9eb0c4
Hugo Deiró <hugodeiro@gmail.com>
2017-01-26 15:32:08 -0300

    Ajustes em funções, acrescentando sobrecarga e sobrescrita

--- codes/002_functions.js
+++ codes/002_functions.js
@@ -66,3 +66,40 @@ var minhaFuncao = function() {
 }
 
 minhaFuncao();
+
+/*
+    JavaScript também permite a criação de funções
+    aninhadas.
+*/
+
+function minhaFuncaoExterna() {
+    console.log('executou minhaFuncaoExterna');
+
+    function minhaFuncaoInterna() {
+        console.log('executou minhaFuncaoInterna');
+    }
+
+    minhaFuncaoInterna();
+}
+
+minhaFuncaoExterna();
+
+/*
+    pode acontecer sobrescrita de funções em 
+    JavaScript
+ */
+
+function minhaFuncaoExterna() {
+    console.log('minhaFuncaoExterna sobrescrita');
+}
+
+/*
+    Contudo, sobrescrita de métodos não é possível devido
+    ao içamento (veja melhor em 004_hoisting.js)
+ */
+
+function minhaFuncaoExterna(parametro) {
+    console.log('minhaFuncaoExterna sobrecarregada', parametro);
+}
+minhaFuncaoExterna(10);
+
