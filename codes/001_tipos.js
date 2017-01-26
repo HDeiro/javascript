08f061d03c508a4094077c6089f31258e0dcb7e1
Hugo Deiró <hugodeiro@gmail.com>
2017-01-26 15:10:04 -0300

    Adicionando código explicando Tipos de Variáveis

---
+++ codes/001_tipos.js
@@ -0,0 +1,32 @@
+/*
+    JavaScript é uma linguagem que possui tipagem dinâmica
+    e, portanto, suas variáveis tem tipos definidos a partir
+    do valor que referencia.
+*/
+
+//Declaração de variável
+var a = 10;
+
+//console.log tem função de exibir alguma mensagem em console
+//typeof tem a função de retornar o tipo de uma determinada variável
+console.log('01 ' + typeof a);
+
+a = 'Estou aprendendo JavaScript';
+
+console.log('02 ' + typeof a);
+
+a = true;
+
+console.log('03 ' + typeof a);
+
+a = new Object();
+
+console.log('04 ' + typeof a);
+
+a = {};
+
+console.log('05 ' + typeof a);
+
+a = [];
+
+console.log('06 ' + typeof a);
