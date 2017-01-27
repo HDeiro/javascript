084f96822a4360a2e138ef27e566bc984f4431ac
Hugo Deiró <hugodeiro@gmail.com>
2017-01-27 20:49:39 -0300

    Adicionando códigos explicando use_strict e correções no arquivo 003_verificacoes.js

--- 003_verificacoes.js
+++ 003_verificacoes.js
@@ -18,17 +18,17 @@
     Segue as mesmas regras da igualdade.
 */
 
-console.log('==== Testes OK ====');
+console.log('---- Testes OK ----');
 console.log(10 == 10);      //Dez é igual a dez?
 console.log(10 % 2 === 0);  //Dez é par? (Verifica se o resto da divisão inteira de 10 por 2 é 0)
 console.log(10 != "11");    //Dez é diferente de onze?
 
-console.log('==== Testes que vão bugar sua mente ====');
+console.log('---- Testes que vão bugar sua mente ----');
 console.log(10 == "10");    //Dez (inteiro) é igual a Dez (String)?
 console.log(1 == true);     //1 (inteiro) equivale a verdadeiro (boolean)?
 console.log('' == false);   //String vazia é false?
 
-console.log('==== Testes vão lhe fazer entender a importância de "===" ====');
+console.log('---- Testes vão lhe fazer entender a importância de "===" ----');
 console.log(10 === "10");    //Dez (inteiro) é igual a Dez (String)?
 console.log(1 === true);     //1 (inteiro) equivale a verdadeiro (boolean)?
 console.log('' === false);   //String vazia é false?
