d49377743db2886ca48a2490e0ad38e153a3fe90
Hugo Deiró <hugodeiro@gmail.com>
2017-01-27 20:55:08 -0300

    Adicionando códigos sobre evaluate

---
+++ 006_evaluate.js
@@ -0,0 +1,9 @@
+/*
+    Com JavaScript é possível executar comandos a partir da
+    função eval(). Ela recebe um parâmetro string que contém
+    um comando a ser executado.
+*/
+
+eval("console.log('Eval funcionando direitinho!')");
+console.log(eval("1 + 2 * 3 - 4 / 5"));
+console.log(eval("eval('2+3')"));
