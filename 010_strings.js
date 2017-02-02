5c8da6d2094337c94b5f75d57250b481dd2de7d4
Hugo Deiró <hugodeiro@gmail.com>
2017-02-02 20:21:40 -0300

    Adicionando códigos de String

---
+++ 010_strings.js
@@ -0,0 +1,39 @@
+/*
+    Existem diversas funções do javascript
+    para tratamento de strings
+*/
+
+var string = "JavaScript vai dominar o mundo!";
+
+//Capturando o tamanho da string
+console.log(string.length);
+
+//Capturando uma substring
+console.log(string.substring(0, 10));
+
+//Retorna a posição de uma substring
+console.log(string.indexOf("mundo"));
+
+//Retorna a última posição de uma substring
+console.log(string.indexOf("m"));
+
+//Coloca todos os textos em caixa alta
+console.log(string.toUpperCase());
+
+//Coloca todos os textos em caixa baixa
+console.log(string.toLowerCase());
+
+//Altera todas partes da string que forem equivalentes ao valor desejado
+console.log(string.replace("mundo", "universo"));
+
+//Divide a string em um array de substrings
+console.log(string.split('mundo'));
+
+//Verifica se a string inicia com um certo nome
+console.log(string.startsWith('JavaScript'));
+
+//Verifica se a string termina com um certo nome
+console.log(string.endsWith('mundo!'));
+
+//Retorna uma nova string com X cópias da string já existente
+console.log(string.repeat(3));
