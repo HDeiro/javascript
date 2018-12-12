06949b0830ca1a46c15ed6686d459005e0618b4e
Hugo <hugodeiro@gmail.com>
2018-12-11 21:27:14 -0300

    Adding example of types on JavaScript

---
+++ 001 - Types/types.js
@@ -0,0 +1,40 @@
+// Undefined
+var a;
+console.log('Here, a is a ' + typeof a);
+
+// Undefined
+var a = undefined;
+console.log('Here, a is a ' + typeof a);
+
+// Number
+a = 10;
+console.log('Here, a is a ' + typeof a);
+
+// String
+a = 'Learning JS';
+console.log('Here, a is a ' + typeof a);
+
+// Boolean
+a = true;
+console.log('Here, a is a ' + typeof a);
+
+// Object
+a = new Object();
+console.log('Here, a is a ' + typeof a);
+
+// Object
+a = {};
+console.log('Here, a is a ' + typeof a);
+
+// Object
+a = [];
+console.log('Here, a is a ' + typeof a);
+
+// Function Object
+a = function() {};
+console.log('Here, a is a ' + typeof a);
+
+// Object with null reference
+a = null;
+console.log('Here, a is a ' + typeof a);
+
