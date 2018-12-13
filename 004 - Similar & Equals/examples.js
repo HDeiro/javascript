d7e4a2c5c98966c02dd7690b329cae4b0f63765e
Hugo <hugodeiro@gmail.com>
2018-12-13 20:55:58 -0300

    Similar & Equals examples

---
+++ 004 - Similar & Equals/examples.js
@@ -0,0 +1,9 @@
+console.log("######### SIMILAR TESTS #########")
+console.log(10 == '10');
+console.log(1 == true);
+console.log('' == false);
+
+console.log("######### EQUAL TESTS #########")
+console.log(10 === '10');
+console.log(1 === true);
+console.log('' === false);
