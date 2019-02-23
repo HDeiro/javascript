9ea8d9565460ef67fa2a3a519ba4b31f35404d9c
Hugo <hugodeiro@gmail.com>
2019-02-23 07:29:11 -0300

    Callback example

---
+++ 011 - Callbacks/script.js
@@ -0,0 +1,18 @@
+// Utilitary function
+function awaitRandom(callback) {
+    let min = 0;
+    let max = 5;
+    //Generate Random number between 5 - 10
+    var rand = Math.floor(Math.random() * (max - min + 1) + min);
+    var awaitTime = rand * 1000;
+    console.log(`Wait for ${rand} seconds`)
+    setTimeout(callback, rand * 1000);
+}
+
+// Callback function
+function callbackFunction() {
+    console.log("########### FINISHED ###########");
+}
+
+// Start
+awaitRandom(callbackFunction);
