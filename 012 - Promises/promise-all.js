1ce7e58f94af5b9a0b53ef3cbebd1f06ee4b010d
Hugo Deiró <hugo.d.souza@ericssoninovacao.com.br>
2019-02-28 14:12:47 -0300

    Corrections in promises

--- 012 - Promises/promise-all.js
+++ 012 - Promises/promise-all.js
@@ -3,18 +3,16 @@ function awaitRandom() {
     let min = 0;
     let max = 5;
     //Generate Random number between 5 - 10
-    var rand = Math.floor(Math.random() * (max - min + 1) + min);
-    var awaitTime = rand * 1000;
+    let rand = Math.floor(Math.random() * (max - min + 1) + min);
+    let awaitTime = rand * 1000;
     // Create promise
     return new Promise((resolve, reject) => {
         console.log(`Wait for ${rand} seconds`);
 
         setTimeout(function() {
             resolve();
-        }, rand * 1000);
+        }, awaitTime);
     });
 }
 
-Promise.all([awaitRandom(), awaitRandom()]).then(_ => {
-    console.log("All promises executed");
-});
+Promise.all([awaitRandom(), awaitRandom()]).then(_ => console.log("All promises executed"));
--- 012 - Promises/promise-success.js
+++ 012 - Promises/promise-success.js
@@ -3,15 +3,15 @@ function awaitRandom() {
     let min = 0;
     let max = 5;
     //Generate Random number between 5 - 10
-    var rand = Math.floor(Math.random() * (max - min + 1) + min);
-    var awaitTime = rand * 1000;
+    let rand = Math.floor(Math.random() * (max - min + 1) + min);
+    let awaitTime = rand * 1000;
     // Create promise
     return new Promise((resolve, reject) => {
         console.log(`Wait for ${rand} seconds`);
 
         setTimeout(function() {
             resolve();
-        }, rand * 1000);
+        }, awaitTime);
     });
 }
 
